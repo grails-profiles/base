d21c7ea78ba956f83807c284f5ae577b800fc505
niravassar <nirav_p_assar@yahoo.com>
2017-05-04 08:41:19 -0500

    testing-support template for domain

--- templates/testing/DomainClass.groovy
+++ templates/testing/DomainClass.groovy
@@ -1,12 +1,9 @@
 @artifact.package@
-import grails.test.mixin.TestFor
+
+import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
-@TestFor(@artifact.name@)
-class @artifact.name@Spec extends Specification {
+class @artifact.name@Spec extends Specification implements DomainUnitTest<@artifact.name@>{
 
     def setup() {
     }
