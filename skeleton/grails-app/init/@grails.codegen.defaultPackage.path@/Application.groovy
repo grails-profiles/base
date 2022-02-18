3031deed03f64a38134a78084deb16cd7075a342
Michael Yan <rain@rainboyan.com>
2022-02-19 00:12:57 +0800

    Grails app should auto import spring component
    Fixed issue: https://github.com/grails/grails-core/issues/12389

--- skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
+++ skeleton/grails-app/init/@grails.codegen.defaultPackage.path@/Application.groovy
@@ -2,10 +2,12 @@ package @grails.codegen.defaultPackage@
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 
 import groovy.transform.CompileStatic
 
 @CompileStatic
+@ComponentScan
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
